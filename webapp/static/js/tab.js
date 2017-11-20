f21bb2eb6caf4908f115c04b0c57f3f0de94e963
Dhruv Sangvikar <dsangvikar@traderscolo.com>
2017-11-19 20:54:12 -0600

    click to play for chart

--- webapp/static/js/tab.js
+++ webapp/static/js/tab.js
@@ -1,153 +1,148 @@
 $( "#tabs" ).tabs();
 
 function myFunc(vars) {
-    return vars
+  return vars
 }
 
 $("#tablink1").click(function () {
-    $.ajax({
-        url:'getData/1',
-        success: function(response) {
-            console.log(response);
-            drawGoogleChart(response,1);
-        }
-    });
-    drawBarGraph();
+  $.ajax({
+    url:'getData/1',
+    success: function(response) {
+      console.log(response);
+      drawGoogleChart(response,1);
+    }
+  });
 })
 
 $("#tablink2").click(function () {
-    $.ajax({
-        url:'getData/2',
-        success: function(response) {
-            console.log(response);
-            drawGoogleChart(response,2)
-        }
-    });
+  $.ajax({
+    url:'getData/2',
+    success: function(response) {
+      console.log(response);
+      drawGoogleChart(response,2)
+    }
+  });
 })
 
 $("#tablink3").click(function () {
-    $.ajax({
-        url:'getData/3',
-        success: function(response) {
-            console.log(response);
-            drawGoogleChart(response,3)
-        }
-    });
+  $.ajax({
+    url:'getData/3',
+    success: function(response) {
+      console.log(response);
+      drawGoogleChart(response,3)
+    }
+  });
 })
 
 $("#tablink4").click(function () {
-    $.ajax({
-        url:'getData/4',
-        success: function(response) {
-            console.log(response);
-            drawGoogleChart(response,4)
-        }
-    });
+  $.ajax({
+    url:'getData/4',
+    success: function(response) {
+      console.log(response);
+      drawGoogleChart(response,4)
+    }
+  });
 })
 
 function drawGoogleChart(response, regId){
-    google.charts.load('current', {
-      'packages':['geochart'],
-      'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-    });
-    google.charts.setOnLoadCallback(drawRegionsMap);
+  google.charts.load('current', {
+    'packages':['geochart'],
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap);
 
-    function drawRegionsMap() {
-      var stateobjects = JSON.parse(response);
-      var chartdata = google.visualization.arrayToDataTable(stateobjects)
+  function drawRegionsMap() {
+    var stateobjects = JSON.parse(response);
+    var chartdata = google.visualization.arrayToDataTable(stateobjects)
 
-      var options = {
-        region: 'US',
-        dataMode: 'regions',
-        resolution: 'provinces',
-        colorAxis: {colors: ['#ead0be', '#d15802']}
-      };
+    var options = {
+      region: 'US',
+      dataMode: 'regions',
+      resolution: 'provinces',
+      colorAxis: {colors: ['#ead0be', '#d15802']}
+    };
 
-      regionId = 'regions_div' + regId
-      var chart = new google.visualization.GeoChart(document.getElementById(regionId));
+    regionId = 'regions_div' + regId
+    var chart = new google.visualization.GeoChart(document.getElementById(regionId));
 
-      chart.draw(chartdata, options);
-    }
+    chart.draw(chartdata, options);
+  }
 }
 
 function drawBarGraph(){
+  google.charts.load('current', {
+    packages: ['corechart', 'bar'],
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+  google.charts.setOnLoadCallback(drawAxisTickColors);
 
-google.charts.load('current', {
-  packages: ['corechart', 'bar'],
-  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-});
-google.charts.setOnLoadCallback(drawAxisTickColors);
-
-function drawAxisTickColors() {
+  function drawAxisTickColors() {
 
     $.ajax({
-        url:'getChartData',
-        success: function(response) {
-            console.log(response);
-            var stateobjects = $.parseJSON(response);
-            var data = google.visualization.arrayToDataTable(stateobjects);
+      url:'getChartData',
+      success: function(response) {
+        console.log(response);
+        var stateobjects = $.parseJSON(response);
+        var data = google.visualization.arrayToDataTable(stateobjects);
 
-            var options = {
-              width: '100%',
-              height: '500px',
-              title: 'Restaurants vs 18+ Population',
-              focusTarget: 'category',
-              hAxis: {
-                title: 'Restaurant Count',
-                format: 'short',
-                viewWindow: {
-                  min: [7, 30, 0],
-                  max: [17, 30, 0]
-                },
-                textStyle: {
-                  fontSize: 14,
-                  color: '#053061',
-                  bold: true,
-                  italic: false
-                },
-                titleTextStyle: {
-                  fontSize: 18,
-                  color: '#053061',
-                  bold: true,
-                  italic: false
-                }
-              },
-              vAxis: {
-                title: 'Population Count',
-                format: 'short',
-                textStyle: {
-                  fontSize: 18,
-                  color: '#67001f',
-                  bold: false,
-                  italic: false
-                },
-                titleTextStyle: {
-                  fontSize: 18,
-                  color: '#67001f',
-                  bold: true,
-                  italic: false
-                }
-              }
-            };
+        var options = {
+          width: '100%',
+          height: '500px',
+          title: 'Restaurants vs 18+ Population',
+          focusTarget: 'category',
+          hAxis: {
+            title: 'Restaurant Count',
+            format: 'short',
+            viewWindow: {
+              min: [7, 30, 0],
+              max: [17, 30, 0]
+            },
+            textStyle: {
+              fontSize: 14,
+              color: '#053061',
+              bold: true,
+              italic: false
+            },
+            titleTextStyle: {
+              fontSize: 18,
+              color: '#053061',
+              bold: true,
+              italic: false
+            }
+          },
+          vAxis: {
+            title: 'Population Count',
+            format: 'short',
+            textStyle: {
+              fontSize: 18,
+              color: '#67001f',
+              bold: false,
+              italic: false
+            },
+            titleTextStyle: {
+              fontSize: 18,
+              color: '#67001f',
+              bold: true,
+              italic: false
+            }
+          }
+        };
 
-            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
-            $('#chart_div').css('background-color','white');
-        }
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+        $('#chart_div').css('background-color','white');
+      }
     });
- 
- // var data = google.visualization.arrayToDataTable([
- //         ['Element', 'Density', { role: 'style' }, { role: 'annotation' } ],
- //         ['Copper', 8.94, '#b87333', 'Cu' ],
- //         ['Silver', 10.49, 'silver', 'Ag' ],
- //         ['Gold', 19.30, 'gold', 'Au' ],
- //         ['Platinum', 21.45, 'color: #e5e4e2', 'Pt' ]
- //      ]);
 
-    }
+  }
 }
 
 $('#click2play').click(function(){
   $('#click2play').hide();
   $("#tablink1").click()
+})
+
+$('#click2playChart').click(function(){
+  $('#click2playChart').hide();
+  drawBarGraph();
 })
